069799950c57bf01e8edb66bf6291e6484f79f45
Grusha <dm1011frl@gmail.com>
2025-09-04 23:12:30 +0300

    Почти настроил добавление элементов в список

--- src/script.js
+++ src/script.js
@@ -12,3 +12,32 @@ headBtn.addEventListener('click', () => {
 infoBtn.addEventListener('click', () => {
     checkClass(infoBlock, "sleep")
 })
+
+
+
+const inputMain = document.querySelector('.input-group__user-input');
+const inputGroupBtn = document.querySelector('.input-group__btn');
+
+
+function createElem() {
+    const listGroup = document.querySelector('.list-group');
+    let elemListWrapper = document.createElement('div');
+    elemListWrapper.classList.add("to-do_list__base", "list-group__elem");
+    let listGroupCheckbox = document.createElement('input');
+    listGroupCheckbox.type = 'checkbox';
+    listGroupCheckbox.name = 'list-checkbox';
+    listGroupCheckbox.classList.add('list-group__elem__checkbox');
+    let elemListText = document.createElement('p');
+    elemListText.classList.add('list-group__elem__text');
+
+    //Сборка
+    elemListWrapper.appendChild(listGroupCheckbox);
+    elemListWrapper.appendChild(elemListText);
+    listGroup.appendChild(elemListWrapper);
+}
+
+inputMain.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        createElem()
+    }
+})
