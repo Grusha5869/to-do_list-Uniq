bfd4edf9ab739939cda3af58fbd2841105f68372
Grusha <dm1011frl@gmail.com>
2025-09-05 23:33:56 +0300

    настроил сохранение в localstorage

--- src/script.js
+++ src/script.js
@@ -17,19 +17,51 @@ infoBtn.addEventListener('click', () => {
 
 const inputMain = document.querySelector('.input-group__user-input');
 const inputGroupBtn = document.querySelector('.input-group__btn');
-
+let listArr = JSON.parse(localStorage.getItem('listContent')) || [];
 
 function createElem() {
     const listGroup = document.querySelector('.list-group');
     let elemListWrapper = document.createElement('div');
     elemListWrapper.classList.add("to-do_list__base", "list-group__elem");
+    elemListWrapper.dataset.id = Date.now();
+    listArr.push({
+        id: elemListWrapper.dataset.id,
+        text: inputMain.value
+    })
+    
+    localStorage.setItem('listContent', JSON.stringify(listArr))
+
     let listGroupCheckbox = document.createElement('input');
     listGroupCheckbox.type = 'checkbox';
     listGroupCheckbox.name = 'list-checkbox';
     listGroupCheckbox.classList.add('list-group__elem__checkbox');
+    
+
     let elemListText = document.createElement('p');
     elemListText.classList.add('list-group__elem__text');
+    elemListText.textContent = inputMain.value;
+    
+    //Сборка
+    elemListWrapper.appendChild(listGroupCheckbox);
+    elemListWrapper.appendChild(elemListText);
+    listGroup.appendChild(elemListWrapper);
+};
 
+function getElem(i) {
+    const listGroup = document.querySelector('.list-group');
+    let elemListWrapper = document.createElement('div');
+    elemListWrapper.classList.add("to-do_list__base", "list-group__elem");
+
+    let listGroupCheckbox = document.createElement('input');
+    listGroupCheckbox.type = 'checkbox';
+    listGroupCheckbox.name = 'list-checkbox';
+    listGroupCheckbox.classList.add('list-group__elem__checkbox');
+    
+
+    let elemListText = document.createElement('p');
+    elemListText.classList.add('list-group__elem__text');
+    elemListText.textContent = listArr[i].text;
+    
     //Сборка
     elemListWrapper.appendChild(listGroupCheckbox);
     elemListWrapper.appendChild(elemListText);
@@ -38,6 +70,34 @@ function createElem() {
 
 inputMain.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        createElem()
+        if (inputMain.value.trim() === "") {
+            inputMain.style.border = '2px solid red';
+        } else {
+            inputMain.style.border = 'none';
+            createElem()
+            inputMain.value = "";
+        }
     }
+});
+inputGroupBtn.addEventListener('click', () => {
+    if (inputMain.value.trim() === "") {
+        inputMain.style.border = '2px solid red';
+    } else {
+        inputMain.style.border = 'none';
+        createElem()
+        inputMain.value = "";
+    }
+});
+/* listGroupCheckbox.addEventListener('change', () => {
+    if (listGroupCheckbox.checked) {
+        console.log(listGroupCheckbox.parentElement);
+        
+    }
+}) */
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < listArr.length; i++) {
+        getElem(i)
+    };
+
 })
